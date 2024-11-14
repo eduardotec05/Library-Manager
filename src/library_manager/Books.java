91b6ab77a059ddc0b091f70b835fbb19ea0ae6b8
eduardotec05 <laloteco123@gmail.com>
2024-11-13 22:19:54 -0600

    add attributes and methods

--- src/library_manager/Books.java
+++ src/library_manager/Books.java
@@ -1,13 +1,70 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package library_manager;
 
-/**
- *
- * @author lalot
- */
 public class Books {
-    
+
+    private String title;
+    private String author;
+    private String editorial;
+    private String ejemplares;
+    private String id;
+    private int stock;
+    private int available;
+//constructors
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAutor(String author) {
+        this.author = author;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public void setEjemplares(String ejemplares) {
+        this.ejemplares = ejemplares;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+
+    //getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public String getEjemplares() {
+        return ejemplares;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
 }
