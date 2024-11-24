3c20600b1265e6a37be574e4bdf4332318adfd32
AngelSH <150072773+AngelSH10004@users.noreply.github.com>
2024-11-24 14:49:39 -0600

    Add files via upload

---
+++ src/library_manager/SaveToTxt.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library_manager;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+/**
+ *
+ * @author angel
+ */
+public class SaveToTxt {
+     public static void saveDataUser(Users user,String filePath) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+            writer.write(user.toString());//Llama el metodo para imprimir los datos ya ordenados 
+            writer.write("---------------\n"); // Separador entre usuarios registrados
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el archivo: " + e.getMessage());
+        }
+    }
+}
